7298ba7339d952bbc88733c8b5b01db2498bf91f
Emma Potiron <emma.potiron@iseg.fr>
2020-06-30 15:24:16 +0200

    J06

---
+++ boucles-suites.php
@@ -0,0 +1,12 @@
+<?php
+
+function compter-dizaine($max)
+{
+    $number=1;
+    while($number=10, $number=100)
+    echo "<p> $number Ceci est une dizaine</p><br>";
+    echo "<p> $number Ceci est une centaine</p><br>";
+    }
+}
+compter-dizaine(100)
+?>
